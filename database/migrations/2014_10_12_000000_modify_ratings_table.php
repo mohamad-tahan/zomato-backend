a16e770727d5b3329fae5ced5c0430c6f0324544
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 21:43:19 +0300

    Modified ratings table by adding pending column

---
+++ database/migrations/2014_10_12_000000_modify_ratings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->string('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
