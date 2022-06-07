9d671b9278b22fa9cc20c2d7f7f904ab32be402f
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 22:10:25 +0300

    Created rating api

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,5 @@ Route::post('/signIn', [UserController::class, 'signIn']);
 Route::get('/restaurantById/{id}', [AdminController::class, 'getRestoById']);
 
 Route::post('/add_review', [UserController::class, 'addReview']);
+
+Route::get('/ratings/{id?}', [AdminController::class, 'getAllReviews']);
