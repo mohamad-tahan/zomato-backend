84c5ed5bbb7331160e6e918ce4d4b6f3525fcf8c
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 19:28:06 +0300

    Createdd function to add and get restaurants

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+
+
+    public function addResto(Request $request){
+        $resto = new Restaurant;
+        $resto->name = $request->name;
+        $resto->description = $request->description;
+        // $resto->image = $request->image;
+        $resto->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
+    
+    public function getAllRestos($id = null){
+        if($id != null){
+            
+            $restos = Restaurant::find($id);
+            //$restos = $restos? $restos->name : '';
+        }else{
+            $restos = Restaurant::all();
+        }
+        
+        return response()->json([
+            "status" => "Success",
+            "restos" => $restos
+        ], 200);
+    }
+
+
 }
