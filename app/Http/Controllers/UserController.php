1fcc07bf49545b38d39df909b1fa767945c0304b
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 19:48:09 +0300

    Added backend for sign up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function signUp(Request $request){
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = hash("sha256", $request->password);
+        $user->usertype_id = 0;
+       
+        $user->save();
+        
+        return response()->json([
+            "status" => "User Saved"
+        ], 200);
+    }
+   
 }
