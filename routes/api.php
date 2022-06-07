8009c1ec1c14663a0ca68f5e3535d8e3974266bb
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 19:49:16 +0300

    Routing add restaurants

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/add_resto', [AdminController::class, 'addResto']);
+
+Route::get('/restaurants/{id?}', [AdminController::class, 'getAllRestos']);
+
+
+Route::post('/add_user', [UserController::class, 'signUp']);
