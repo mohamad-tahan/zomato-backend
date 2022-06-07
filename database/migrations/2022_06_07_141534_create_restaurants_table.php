de07c94ed99f631e07e321a8542c977b5b93f1d0
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-07 18:26:08 +0300

    Incremeting id

--- database/migrations/2022_06_07_141534_create_restaurants_table.php
+++ database/migrations/2022_06_07_141534_create_restaurants_table.php
@@ -14,7 +14,7 @@ class CreateRestaurantsTable extends Migration
     public function up()
     {
         Schema::create('restaurants', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('description');
             $table->binary('image');
